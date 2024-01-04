4aa82f0ce283699422031808631a4607f65b2c33
Olena-Ignatenko <128995746+Olena-Ignatenko@users.noreply.github.com>
2024-01-04 21:10:44 +0000

    Deploying to gh-pages from @ Olena-Ignatenko/goit-js-hw-09@b08f185bd56e73c9e820fec0298977148a902f8a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function r(){return`
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */function n(){return`
     <form class="feedback-form" autocomplete="off">
       <label>
         Email
@@ -10,5 +10,5 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
       </label>
       <button type="submit">Submit</button>
     </form>
-  `}const m=document.getElementById("form-container");m.innerHTML=r();const l=document.querySelector(".feedback-form"),n="feedback-form-state";l.addEventListener("input",s);l.addEventListener("submit",i);let e=JSON.parse(localStorage.getItem(n))||{};const{email:t,message:a}=l.elements;u();function s(o){e={email:t.value,message:a.value},localStorage.setItem(n,JSON.stringify(e))}function u(){e&&(t.value=e.email||"",a.value=e.message||"")}function i(o){if(o.preventDefault(),console.log({email:t.value,message:a.value}),t.value===""||a.value==="")return alert("Please fill in all the fields!");localStorage.removeItem(n),o.currentTarget.reset(),e={}}
+  `}const r=document.getElementById("form-container");r.innerHTML=n();const o=document.querySelector(".feedback-form"),m="feedback-form-state";o.addEventListener("input",s);o.addEventListener("submit",c);let t=i();function s(a){const{email:e,message:l}=o.elements;t={email:e.value.trim(),message:l.value.trim()},localStorage.setItem(m,JSON.stringify(t))}function i(){return JSON.parse(localStorage.getItem(m))||{}}function u(){const{email:a,message:e}=o.elements;t&&(a.value=t.email||"",e.value=t.message||"")}function c(a){a.preventDefault();const{email:e,message:l}=o.elements;if(e.value.trim()===""||l.value.trim()==="")return alert("Please fill in all the fields!");console.log({email:e.value,message:l.value}),localStorage.removeItem(m),a.currentTarget.reset(),t={}}u();
 //# sourceMappingURL=commonHelpers2.js.map
